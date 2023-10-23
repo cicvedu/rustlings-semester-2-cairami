33ce141578de11fe3f2a7dfa19553e01892984bb
lcai001 <st96cail@163.com>
2023-10-23 09:47:08 +0800

    update

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -15,7 +15,10 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(success_value) => success_value,
+        Err(_) => { println!("invalid digit found in string"); return () }
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
