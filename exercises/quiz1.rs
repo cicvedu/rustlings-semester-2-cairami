f83b4683bda18af01c79281eb1ad687f65f8e1b5
lcai001 <st96cail@163.com>
2023-10-23 20:52:50 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(rustbucks: u16) -> u16 {
+    if rustbucks > 40 {
+        rustbucks
+    } else {
+        rustbucks * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
