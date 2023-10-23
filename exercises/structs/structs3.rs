ce0efce85f22dd048de40a361120c5f44038d99c
lcai001 <st96cail@163.com>
2023-10-23 12:04:39 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,17 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        self.recipient_country != self.sender_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        if cents_per_gram < 0 {
+            panic!("cents per gram should not be less than 0!");
+        }
+        self.weight_in_grams * cents_per_gram
     }
 }
 
