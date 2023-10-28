512b1ac1142a3e717b2438ee6d364910c05d579d
lcai001 <st96cail@163.com>
2023-10-28 12:53:38 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -31,10 +31,10 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
-    unsafe {
-        let value: &mut u32 = &mut (address as u32);
+    // unsafe {
+        let value = &mut *(address as *mut u32);
         *value = 0xAABBCCDD;
-    }
+    //}
 }
 
 #[cfg(test)]
